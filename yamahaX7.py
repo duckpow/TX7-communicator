0910a62030e0fc86e57425093e6347d6f9fd8e83
duckpow <duckpow@gmail.com>
2014-09-20 16:09:12 +0200

    Created operator class

--- yamahaX7.py
+++ yamahaX7.py
@@ -7,6 +7,7 @@ from midiSuperDevice import MidiDevice
 class TX7(MidiDevice):
 	"""Handler class for connections to TX7 or DX7"""
 
+	operators = []
 	get_patch_message = []
 
 	current_patch_name = ""
@@ -15,6 +16,10 @@ class TX7(MidiDevice):
 		self.input_id = input_id
 		self.output_id = output_id
 		MidiDevice.__init__(self,input_id,output_id)
+
+		for i in range(6):
+			self.operators.append(TX7Operator())
+
 		#Get current patch on start up
 		#This message requests patch data from TX7 (should work on DX7 as well)
 		self.get_patch_message = [self.SYSEX_START_ID, self.YAMAHA_ID, 32, 0, self.SYSEX_EOX_ID]
@@ -72,3 +77,56 @@ class TX7(MidiDevice):
 				print(midiMsg)
 		else:
 			return 0
+
+class TX7Operator(object):
+
+	on = False
+	eg_rate = [0,0,0,0]
+	eg_lvl = [0,0,0,0]
+	key_lvlScale_array = [0,0,0,0,0]
+	key_rateScale = 0
+	mod_sens_Amp = 0
+	key_vel_sens = 0
+	output_lvl = 0
+	osc_mode = 0
+	freq_array = [0,0,0]
+
+	allParam = []
+
+	def __init__(self):
+		self.on=1
+
+	def set_on(self,msg):
+		if isinstance(msg,bool):
+			self.on = msg
+
+	def setParam(self,data):
+		if isinstance(data,list):
+			if len(data) == 21:
+				self.eg_rate = data[0:4]
+				self.eg_lvl = data[4:8]
+				self.key_lvlScale_array = data[8:13]
+				self.key_rateScale = data[13]
+				self.mod_sens_Amp = data[14]
+				self.key_vel_sens = data[15]
+				self.output_lvl = data[16]
+				self.osc_mode = data[17]
+				self.freq_array = data[18,21]
+
+	def getParam(self):
+		allParam = []
+		for i in self.eg_rate:
+			self.allParam.append(i)
+		for i in self.eg_lvl:
+			self.allParam.append(i)
+		for i in self.key_lvlScale_array:
+			self.allParam.append(i)
+		self.allParam.append(self.key_rateScale)
+		self.allParam.append(self.mod_sens_Amp)
+		self.allParam.append(self.key_vel_sens)
+		self.allParam.append(self.output_lvl)
+		self.allParam.append(self.osc_mode)
+		for i in self.freq_array:
+			self.allParam.append(i)
+		return self.allParam
+
