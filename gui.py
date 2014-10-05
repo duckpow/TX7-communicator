a970f9a99f0e128630e03150c7e3e1a297f832af
duckpow <duckpow@gmail.com>
2014-10-05 16:10:37 +0200

    Created LFO widget. Refactored sliders to class

--- gui.py
+++ gui.py
@@ -13,10 +13,95 @@ from PyQt4.QtCore import SIGNAL
 from yamahaX7 import TX7
 from midiController import MidiController
 
+class SliderWithNameAndLabel(QtGui.QWidget):
+	"""Creates a slider with name and value label and connects it to the tx7"""
+	def __init__(self, name, param_number, minMax=[0,99],**kwargs):
+		super(SliderWithNameAndLabel, self).__init__()
+
+		if kwargs is not None:
+			if 'horizontal' in kwargs.keys():
+				if isinstance(kwargs['horizontal'],bool):
+					self.horizontal = kwargs['horizontal']
+			elif 'vertical' in kwargs.keys():
+				if isinstance(kwargs['vertical'],bool):
+					self.horizontal = not kwargs['vertical']
+			else:
+				self.horizontal = True
+
+		self.vertical = not self.horizontal
+
+		self.name = name
+		self.param_number = param_number
+
+		self.grid = QtGui.QGridLayout()
+		self.setLayout(self.grid)
+
+		self.label = QtGui.QLabel(name)
+		self.grid.addWidget(self.label,0 * self.vertical,0 * self.horizontal)
+
+		if self.horizontal:
+			self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+		else:
+			self.slider = QtGui.QSlider()
+
+		self.slider.setRange(minMax[0],minMax[1])
+		self.grid.addWidget(self.slider,1 * self.vertical,1 * self.horizontal)
+		self.slider.sliderReleased.connect(self.sendParam)
+
+		self.valueLabel = QtGui.QLabel()
+		self.grid.addWidget(self.valueLabel,2 * self.vertical,2 * self.horizontal)
+		self.slider.valueChanged.connect(self.valueLabel.setNum)
+
+	def sendParam(self):
+		app.tx7.write_param(self.param_number,self.slider.value())
+
+	def updateParam(self, value):
+		self.slider.setValue(value)
+
+
+class ToggleWithName(QtGui.QWidget):
+	"""docstring for ToggleWithName"""
+	def __init__(self, name, param_number):
+		super(ToggleWithName, self).__init__()
+		self.name = name
+		self.param_number = param_number
+
+		self.grid = QtGui.QGridLayout()
+		self.setLayout(self.grid)
+
+		self.label = QtGui.QLabel(name)
+		self.grid.addWidget(self.label,0,0)
+
+		self.toggle = QtGui.QRadioButton()
+		self.grid.addWidget(self.toggle,1,0)
+		self.toggle.released.connect(self.sendParam)
+
+	def sendParam(self):
+		self.tog = self.sender()
+		app.tx7.write_param(self.param_number,int(self.tog.isChecked()))
+
+	def updateParam(self, checked):
+		if isinstance(checked,bool):
+			self.toggle.setChecked(checked)
+		elif isinstance(checked,int):
+			self.toggle.setChecked(bool(checked))
+		else:
+			print("Must be bool or int")
+		
+
 class IdentifiableQSlider(QtGui.QSlider):
-	"""Addition of index to QSlider that makes it identifiable from a signal"""
+	"""Addition of index to QSlider that makes it identifiable for a signal"""
 	def __init__(self, index):
 		super(IdentifiableQSlider, self).__init__()
+		self.index = index
+
+	def getIndex(self):
+		return self.index
+
+class IdentifiableQDial(QtGui.QDial):
+	"""Addition of index to QDial that makes it identifiable for a signal"""
+	def __init__(self, index):
+		super(IdentifiableQDial, self).__init__()
 		self.index = index
 
 	def getIndex(self):
@@ -49,8 +134,6 @@ class Envelope(QtGui.QWidget):
 		self.rateLabel = QtGui.QLabel("Rates")
 		self.grid.addWidget(self.rateLabel,1,0,1,2)
 
-
-
 		for i in range(self.points):
 			self.rates[i].setRange(0,99)
 			self.grid.addWidget(self.rates[i],2,i)
@@ -58,7 +141,7 @@ class Envelope(QtGui.QWidget):
 			self.rates[i].valueChanged.connect(self.rateLabels[i].setNum)
 			self.rates[i].sliderReleased.connect(self.sendParam)
 
-		self.grid.setRowMinimumHeight(4,10)
+		#self.grid.setRowMinimumHeight(4,10)
 
 		self.levelLabel = QtGui.QLabel("Levels")
 		self.grid.addWidget(self.levelLabel,4,0,1,2)
@@ -97,6 +180,59 @@ class TX7Operator(QtGui.QWidget):
 	def sendParam(self):
 		pass
 
+class LFO(QtGui.QWidget):
+	"""LFO Widget. Only used once... but this makes things easier to comprehend"""
+	def __init__(self,):
+		super(LFO, self).__init__()
+		
+		self.grid = QtGui.QGridLayout()
+		self.setLayout(self.grid)
+
+		self.names = ["Speed", "Delay", "Pitch Mod", "Amp Mod"]
+		self.nameLabels = []
+		self.dials = []
+		self.valueLabels = []
+
+		self.headerLabel = QtGui.QLabel("LFO parameters")
+		self.grid.addWidget(self.headerLabel,0,0,1,2)
+
+		for index in range(len(self.names)):
+			self.current_row = (index/2)*3 + 1
+			self.current_column = index%2
+			self.nameLabels.append(QtGui.QLabel(self.names[index]))
+			self.grid.addWidget(self.nameLabels[index],self.current_row, self.current_column)
+
+			self.dials.append(IdentifiableQDial(index))
+			self.dials[index].setRange(0,99)
+			self.grid.addWidget(self.dials[index],self.current_row+1, self.current_column)
+			self.dials[index].sliderReleased.connect(self.sendParam)
+
+			self.valueLabels.append(QtGui.QLabel())
+			self.grid.addWidget(self.valueLabels[index],self.current_row+2, self.current_column)
+			self.dials[index].valueChanged.connect(self.valueLabels[index].setNum)
+
+		self.lfoSync = ToggleWithName("LFO Sync", 141)
+		self.grid.addWidget(self.lfoSync,1,2)
+
+		self.waveForm = QtGui.QComboBox()
+		self.grid.addWidget(self.waveForm,2,2,1,2)
+		self.waveForm.addItems(["Triangle","Saw","Rev Saw","Square","Sine"])
+		self.waveForm.activated[int].connect(self.sendParamWithArg)
+
+	def sendParamWithArg(self, i):
+		app.tx7.write_param(142,i)
+
+	def sendParam(self):
+		self.dial = self.sender()
+		app.tx7.write_param(self.dial.getIndex()+137,self.dial.value())
+
+	def updateParam(self, lfoParamArray):
+		for i in range(4):
+			self.dials[i].setValue(lfoParamArray[i])
+		self.lfoSync.updateParam(lfoParamArray[4])
+		self.waveForm.setCurrentIndex(lfoParamArray[5])
+		
+
 class MainWindow(QtGui.QWidget):
 	"""MainWindow for displaying and changing parameters"""
 	def __init__(self):
@@ -116,20 +252,20 @@ class MainWindow(QtGui.QWidget):
 		self.grid.addWidget(self.name_box,0,1)
 
 		#Algorithm
-		self.algorithm_text = QtGui.QLabel('Algorithm:')
-		self.grid.addWidget(self.algorithm_text,1,0)
-		self.algorithm_slider = QtGui.QSlider(QtCore.Qt.Horizontal)
-		self.algorithm_slider.setMinimum(0)
-		self.algorithm_slider.setMaximum(31)
-		self.algorithm_slider.sliderReleased.connect(self.algorithmChanged)
-		self.grid.addWidget(self.algorithm_slider,1,1)
-		self.algorithm_value = QtGui.QLabel()
-		self.algorithm_value.setNum(self.algorithm_slider.value())
-		self.grid.addWidget(self.algorithm_value,1,2)
+		self.algorithm = SliderWithNameAndLabel("Algorithm",134,minMax=[0,31],horizontal=True)
+		self.grid.addWidget(self.algorithm,1,0,1,3)
+
+		#Misc
+		self.feedback = SliderWithNameAndLabel("Feedback",135,minMax=[0,7],vertical=True)
+		self.grid.addWidget(self.feedback,0,3,3,1)
 
 		#Pitch Envelope
 		self.pitchEnv = Envelope(126)
 		self.grid.addWidget(self.pitchEnv,2,0)
+
+		#LFO
+		self.lfo = LFO()
+		self.grid.addWidget(self.lfo,2,1)
 
 		self.setGeometry(500,500,550,550)
 		self.setWindowTitle('TX7 editor')
@@ -155,9 +291,12 @@ class MainWindow(QtGui.QWidget):
 			Goes trough the TX7 object and and updates the data (once finished)
 		'''
 		self.name_box.setText(app.tx7.current_patch_name)
-		self.algorithm_slider.setValue(app.tx7.algorithm)
-		self.algorithm_value.setNum(self.algorithm_slider.value())
+		self.algorithm.updateParam(app.tx7.algorithm)
+		self.feedback.updateParam(app.tx7.feedback)
+		#self.algorithm_slider.setValue(app.tx7.algorithm)
+		#self.algorithm_value.setNum(self.algorithm_slider.value())
 		self.pitchEnv.updateParam(app.tx7.pitch_eg_rate,app.tx7.pitch_eg_lvl)
+		self.lfo.updateParam(app.tx7.LFO)
 
 	def patchNameChanged(self):
 		app.tx7.change_name(str(self.name_box.text()))
@@ -281,13 +420,12 @@ class App(Qt.QApplication):
 			#Data should always be read if poll returns true
 			self.data = self.tx7.read()
 			self.mainWindow.updateFromData()
-			print("Recieved from TX7")
+			# print("Recieved from TX7") #debugging
 		elif self.keyboard.poll():
-			self.ledTimer = 30
 			#self.mainWindow.midiReceived()
 			self.data = self.keyboard.read()
 			self.tx7.write(self.data)
-			print("Recieved from keyboard" + str(self.data))
+			#print("Recieved from keyboard" + str(self.data)) #debugging
 
 	def byebye(self):
 		self.exit(0)
